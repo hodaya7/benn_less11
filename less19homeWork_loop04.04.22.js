6e5e4a8fbd57db0a2adc0161c849ba2cfc0d0707
hodaya7 <hodayabig@gmail.com>
2024-05-20 11:50:09 +0300

    loop

---
+++ less19homeWork_loop04.04.22.js
@@ -0,0 +1,240 @@
+//for
+//1
+function printNumber() {
+    for (let i = 0; i < 10; i++)
+        console.log(i + 1);
+}
+
+printNumber()
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// 10
+
+//2
+function deleteElement(arr, number) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == number)
+            delete arr[i];
+    }
+}
+
+var arr = [25, 56, 4, 78, 5, 63, 45, 210, 56];
+deleteElement(arr, 4);
+console.log(arr);
+// [ 25, 56, <1 empty item>, 78, 5, 63, 45, 210, 56 ]
+
+//3
+function oddEven(arr) {
+    var ob = {
+        oddsNum: 0,
+        evensNum: 0
+    }
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 == 0)
+            ob.evensNum++;
+        else
+            ob.oddsNum++;
+    }
+    return ob;
+}
+
+console.log(oddEven([1, 2, 3, 4, 6]));//{ oddsNum: 2, evensNum: 3 }
+
+//4
+function printPyramid(num) {
+    for (let i = 0; i < num; i++) {
+        for (let j = 0; j < i + 1; j++) {
+            process.stdout.write("*");
+        }
+        console.log();
+    }
+}
+
+printPyramid(5);
+// *
+// **
+// ***
+// ****
+// *****
+
+//5
+function findDup(strArr) {
+    var counts = []
+    for (let i = 0; i < strArr.length; i++) {
+        counts[strArr[i]] = 0;
+    }
+    for (let i = 0; i < strArr.length; i++) {
+        counts[strArr[i]]++;
+    }
+
+    var obj = {};
+    for (let i in counts) {
+        if (counts[i] > 1)
+            obj[i] = counts[i];
+    }
+    return obj;
+}
+
+console.log(findDup([1, 2, 3, 2, 6, 5, 6, 6]));//{ '2': 2, '6': 3 }
+
+//6
+//א
+for (let i = 0; i < 20; i++)
+    console.log(i + 1);
+
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// 10
+// 11
+// 12
+// 13
+// 14
+// 15
+// 16
+// 17
+// 18
+// 19
+// 20    
+console.log("===ב===");
+for (let i = 3; i < 30; i += 2) {
+    console.log(i);
+}
+// 3
+// 5
+// 7
+// 9
+// 11
+// 13
+// 15
+// 17
+// 19
+// 21
+// 23
+// 25
+// 27
+// 29
+console.log("===ג===");
+for (let i = 20; i > 1; i -= 2) {
+    console.log(i);
+}
+// ===ג===
+// 20
+// 18
+// 16
+// 14
+// 12
+// 10
+// 8
+// 6
+// 4
+// 2
+console.log("===ד===");
+for (let i = 48; i > 20; i -= 3) {
+    console.log(i);
+}
+// ===ד===
+// 48
+// 45
+// 42
+// 39
+// 36
+// 33
+// 30
+// 27
+// 24
+// 21
+
+//7
+var l = "LaunchCode";
+var arr = [1, 5, "LC101"];
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+// 1
+// 5
+// LC101
+for (let i = l.length - 1; i >= 0; i--) {
+    console.log(l[i]);
+}
+// e
+// d
+// o
+// C
+// h
+// c
+// n
+// u
+// a
+// L
+
+//while
+//8
+var i = 0;
+while (i < 10) {
+    console.log("Hello");
+    i++;
+}
+
+// Hello
+// Hello
+// Hello
+// Hello
+// Hello
+// Hello
+// Hello
+// Hello
+// Hello
+// Hello
+
+//9
+function sumNum(arr, num) {
+    i = 0;
+    var sum = 0;
+    while (sum < num && i < arr.length) {
+        sum += arr[i];
+        i++;
+    }
+    return sum;
+}
+
+console.log(sumNum([1,2,3,4], 5));//6
+
+//10
+function falseIndex(arr) {
+    i = 0;
+    while (i<arr.length) {
+        if(!arr[i])
+        return i;
+        i++;
+    }
+    return -1;
+}
+
+console.log(falseIndex([true,true, false, false]));//2
+
+//10
+function isHello(arr) {
+    i=0;
+    while (i<arr.length) {
+       if(i%2!=0&&arr[i]=="Hello")
+       return i;
+        i++;
+    }
+    return -1;
+}
+
+console.log(isHello(["ok", "love"," Hello","Hello"]));//3
---
+++ less19loop04.04.22.js
@@ -0,0 +1,72 @@
+//הדפסה לפי כמות פעמים
+function dup(str, num) {
+    console.log(str.repeat(num));
+}
+
+dup("aaa ", 5)//aaa aaa aaa aaa aaa 
+
+function print(element, index) {
+    console.log(element);
+}
+
+["hi ", "hi ", "hi "].forEach(print);
+// hi 
+// hi
+// hi
+
+function dupFor(str, num) {
+    for (let i = 0; i < num; i++) //כשידוע לרוב מתי מסתיימת הלולאה
+        console.log(str);
+}
+
+dupFor("ggg", 7)
+// ggg
+// ggg
+// ggg
+// ggg
+// ggg
+// ggg
+// ggg
+
+function printArr(strArr) {
+    for (let i = strArr.length - 1; i >= 0; i--)
+        console.log(strArr[i]);
+}
+
+printArr(["orange", "apple", "banana", "melon"])
+// melon
+// banana
+// apple
+// orange
+
+//פונקציה שמדפיסה מערך הפוך בforeach
+function printRev(element, index) {
+    console.log(array[array.length - 1 - index]);
+}
+var array = [1, 2, 3, 4, 5];
+array.forEach(printRev)// Output: 5 4 3 2 1
+
+console.log("======2=========");
+function addElementToBackwardArr(element, index) {
+    backArr[array.length - 1 - index] = element;
+}
+
+// דוגמה לשימוש
+var backArr = []
+array.forEach(addElementToBackwardArr)// Output: 5 4 3 2 1
+console.log(backArr);
+
+//while
+function printArr(arr) {
+    var i = 0;
+    while (i < arr.length) {
+        console.log(arr[i]);
+        i++;
+    }
+}
+
+var ar = [4, 5, 6]
+printArr(ar)
+// 4
+// 5
+// 6
